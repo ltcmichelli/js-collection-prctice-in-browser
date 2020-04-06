e55c39b71cf043240853b01df805aa09a92072e2
ltcmichelli <42404448+ltcmichelli@users.noreply.github.com>
2020-04-06 11:30:48 +0800

    practice 3-2

--- practice/section-3/practice-2.js
+++ practice/section-3/practice-2.js
@@ -1,9 +1,9 @@
 'use strict';
 
 function createUpdatedCollection(collectionA, objectB) {
-  const keyB = Object.values(objectB);
+  let arrayOfObjectB = Object.values(objectB).flat();
   return collectionA.map(element => {
-    if (keyB[0].includes(element.key)) {
+    if (arrayOfObjectB.includes(element.key)) {
       return {
         key: element.key,
         count: element.count - Math.floor(element.count / 3)
