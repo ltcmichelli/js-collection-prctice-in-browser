1ad412639c42f0c3069ae5204fc87cb1a3fa54a4
ltcmichelli <42404448+ltcmichelli@users.noreply.github.com>
2020-04-06 11:17:52 +0800

    practice 1-4

--- practice/section-1/practice-4.js
+++ practice/section-1/practice-4.js
@@ -1,12 +1,8 @@
 'use strict';
 
 function collectSameElements(collectionA, objectB) {
-  const keyA = [];
-  for (let [key, value] of Object.entries(collectionA)) {
-    keyA.push(Object.values(value)[0]);
-  }
-  
-  const keyB = Object.values(objectB);
-  
-  return keyA.filter(element =>keyB[0].includes(element));
+  let flattedArrayOfObjectB = Object.values(objectB).flat();
+  let filteredCollectionA = collectionA.filter(element => flattedArrayOfObjectB.includes(element.key));
+
+  return filteredCollectionA.map(element => element.key);
 }
